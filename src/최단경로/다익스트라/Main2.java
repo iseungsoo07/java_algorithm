ed29914ea8779cbe1417b5accc8f702133e3e6ca
iseungsoo07 <iseungsoo07@gmail.com>
2023-08-06 22:17:36 +0900

    최단경로, 최소신장트리 개념 + 연습문제

---
+++ src/최단경로/다익스트라/Main2.java
@@ -0,0 +1,71 @@
+package 최단경로.다익스트라;// 다익스트라 우선순위 큐 사용
+
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class Main2 {
+    static class Node {
+        int to;
+        int weight;
+
+        public Node(int to, int weight) {
+            this.to = to;
+            this.weight = weight;
+        }
+    }
+
+    public static void dijkstra(int v, int[][] data, int start) {
+        ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+        for (int i = 0; i < v + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < data.length; i++) {
+            graph.get(data[i][0]).add(new Node(data[i][1], data[i][2]));
+        }
+
+        int[] dist = new int[v + 1];
+        for (int i = 1; i < v + 1; i++) {
+            dist[i] = Integer.MAX_VALUE;
+        }
+
+        dist[start] = 0;
+
+        PriorityQueue<Node> pq = new PriorityQueue<>((x, y) -> x.weight - y.weight);
+        pq.offer(new Node(start, 0));
+
+        while (!pq.isEmpty()) {
+            Node curNode = pq.poll();
+
+            if (dist[curNode.to] < curNode.weight) {
+                continue;
+            }
+
+            for (int i = 0; i < graph.get(curNode.to).size(); i++) {
+                Node adjNode = graph.get(curNode.to).get(i);
+
+                if (dist[adjNode.to] > curNode.weight + adjNode.weight) {
+                    dist[adjNode.to] = curNode.weight + adjNode.weight;
+                    pq.offer(new Node(adjNode.to, dist[adjNode.to]));
+                }
+            }
+        }
+
+
+        for (int i = 1; i < v + 1; i++) {
+            if (dist[i] == Integer.MAX_VALUE) {
+                System.out.print("INF ");
+            } else {
+                System.out.print(dist[i] + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Test code
+        int[][] data = {{1, 2, 2}, {1, 3, 3}, {2, 3, 4}, {2, 4, 5}, {3, 4, 6}, {5, 1, 1}};
+        dijkstra(5, data, 1);
+    }
+}
---
+++ src/최단경로/플로이드워셜/Main.java
@@ -0,0 +1,59 @@
+package 최단경로.플로이드워셜;
+
+// 알고리즘 - 최단 경로 알고리즘
+// 플로이드-워셜
+
+public class Main {
+
+    static int[][] dist;
+    static final int INF = 1000000000;
+
+    public static void floydWarshall(int v, int e, int[][] data, int start) {
+        dist = new int[v + 1][v + 1];
+
+        // dist 배열 초기화
+        for (int i = 1; i < v + 1; i++) {
+            for (int j = 1; j < v + 1; j++) {
+                if (i != j) {
+                    dist[i][j] = INF;
+                }
+            }
+        }
+
+        for (int i = 0; i < e; i++) {
+            dist[data[i][0]][data[i][1]] = data[i][2];
+        }
+
+        for (int k = 1; k < v + 1; k++) {
+            // i -> j (k를 거쳐서 가는 경우가 짧을 때 업데이트)
+            for (int i = 1; i < v + 1; i++) {
+                for (int j = 1; j < v + 1; j++) {
+                    if (dist[i][k] != INF && dist[k][j] != INF) {
+                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i < v + 1; i++) {
+            for (int j = 1; j < v + 1; j++) {
+                if (dist[i][j] >= INF) {
+                    System.out.printf("%5s ", "INF");
+                } else {
+                    System.out.printf("%5d ", dist[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        // Test code
+        int[][] data = {{1, 2, 8}, {1, 3, 6}, {1, 5, 5}, {2, 3, -5}, {2, 4, 1}, {2, 6, 4}, {3, 4, 4}, {4, 7, 3}, {5, 6, 5}, {6, 2, 0}, {6, 7, -7}};
+        floydWarshall(7, 11, data, 1);
+        System.out.println();
+
+        data = new int[][]{{1, 2, 8}, {1, 3, 6}, {1, 5, 5}, {2, 3, -5}, {2, 4, 1}, {2, 6, 4}, {3, 4, 4}, {4, 7, 3}, {5, 6, 5}, {6, 2, -5}, {6, 7, -7}};
+        floydWarshall(7, 11, data, 1);
+    }
+}
